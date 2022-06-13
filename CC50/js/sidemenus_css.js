1c921f4283f2e9da204c9fcbb17b86e5db79c3ea
PEDRO MARCHET <100133140+pedromarchet01@users.noreply.github.com>
2022-06-13 15:03:35 -0300

    Add files via upload

---
+++ CC50/js/sidemenus_css.js
@@ -0,0 +1,9 @@
+var body = document.querySelector("body");
+body.style.backgroundColor = "black";
+
+var nav = document.querySelector("nav");
+nav.style.color = "black";
+nav.style.fontFamily = "helvetica";
+
+
+
